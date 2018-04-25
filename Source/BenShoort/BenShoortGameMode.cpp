44277481217307faeb180b91c4bbb898eed6dbee
5ZV6Y17SPELZPY1\Administrator <1194438562@qq.com>
2018-04-25 15:55:57 +0800

    更改了文件结构重新命名了Cpp文件 , 让目录和C++更加清晰

--- Source/BenShoort/BenShoortGameMode.cpp
+++ Source/BenShoort/BenShoortGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "BenShoortGameMode.h"
 #include "BenShoortHUD.h"
-#include "BenShoortCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ABenShoortGameMode::ABenShoortGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
